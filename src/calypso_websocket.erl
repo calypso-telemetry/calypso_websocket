cf14b9927f01e33ab7e27672e6b8105ddbca8049
Sergey Loguntsov <loguntsov@gmail.com>
2015-06-24 12:17:36 +0300

    Support multiple handlers

--- src/calypso_websocket.erl
+++ src/calypso_websocket.erl
@@ -3,8 +3,11 @@
 
 %% API
 -export([
-  cowboy_route/3
+  cowboy_route/3,
+  add_handler/1, remove_handler/1
 ]).
+
+-define(APP, calypso_websocket).
 
 cowboy_route(Module, Url, Opt) when is_atom(Module), is_list(Url) ->
   { Url, calypso_websocket_handler, { Module, Opt }}.
--- src/calypso_websocket_handler.erl
+++ src/calypso_websocket_handler.erl
@@ -13,11 +13,13 @@
 
 -define(TICK, 30).
 
--record(req, {
-	module :: atom(),
+-record(state, {
+	modules :: [atom()],
 	ref :: reference(),
 	ping_time :: calypso_time:time(),
-	state :: term()
+	states :: [{ atom(), term()}],
+	decode_fun :: fun((term()) -> term()),
+	encode_fun :: fun((term()) -> term())
 }).
 
 -type frame() :: close | ping | pong
@@ -46,24 +48,27 @@
 
 -callback terminate(Reason, Req, State) -> ok.
 
-init(Req, { Module, Opts}) when is_atom(Module) ->
-	{ ok, St } = Module:init(Req, Opts),
-	State = tick(#req{
-		module = Module,
+init(Req, { Modules, Opts}) ->
+	ModulesSt = lists:reverse(lists:foldl(fun(Module, Acc) when is_atom(Module) ->
+		{ ok, St } = Module:init(Req, Opts),
+		[ {Module, St } | Acc ]
+	end, [], Modules)),
+	State = tick(#state{
+		modules = Modules,
 		ref = make_ref(),
-		state = St
+		states = ModulesSt
 	}),
-	{cowboy_websocket, Req, State#req{
+	{cowboy_websocket, Req, State#state{
 		ping_time = undefined
 	}}.
 
 websocket_handle({text, <<"pong">>}, Req, State) ->
-	{ ok, Req, State#req{
+	{ ok, Req, State#state{
 		ping_time = undefined
 	}};
 
 websocket_handle({text, <<"ping">>}, Req, State) ->
-	{ reply, { text, <<"pong">> }, Req, State#req{
+	{ reply, { text, <<"pong">> }, Req, State#state{
 	  ping_time = calypso_time:now()
 	}};
 
@@ -82,12 +87,12 @@ websocket_handle(Msg, Req0, State) ->
 			{shutdown, Req, set_st(St, State)}
 	end.
 
-websocket_info({ tick, Ref },  Req, State#req{ ping_time = undefined }) when Ref =:= State#req.ref ->
-	{ reply, { text, <<"ping">> }, Req, tick(State#req{
+websocket_info({ tick, Ref },  Req, State#state{ ping_time = undefined }) when Ref =:= State#state.ref ->
+	{ reply, { text, <<"ping">> }, Req, tick(State#state{
 		ping_time = calypso_time:now()
 	})};
 
-websocket_info({ tick, Ref },  Req, State#req{ ping_time = Time }) when Ref =:= State#req.ref ->
+websocket_info({ tick, Ref },  Req, State#state{ ping_time = Time }) when Ref =:= State#state.ref ->
 	case calypso_time:diff(Time + ?TICK, calypso_time:now()) < 0 of
 		true ->
 			{ shutdown, Req, State };
@@ -96,34 +101,68 @@ websocket_info({ tick, Ref },  Req, State#req{ ping_time = Time }) when Ref =:= 
 	end;
 
 websocket_info(Msg, Req0, State) ->
-	Module = module(State),
-	case Module:websocket_info(Msg, Req0, st(State)) of
-		{ok, Req, St} ->
-			{ ok, Req, set_st(St, State) };
-		{ok, Req, St, hibernate} ->
-			{ ok, Req, set_st(St, State), hibernate};
-		{reply, Frames, Req, St} ->
-			{reply, Frames, Req, set_st(St, State) };
-		{reply, Frames, Req, St, hibernate} ->
-			{reply, Frames, Req, set_st(St, State), hibernate};
-		{shutdown, Req, St} ->
-			{shutdown, Req, set_st(St, State)}
+	case apply_cmd(fun(Module, Msg, Req, St) ->
+		Module:websocket_info(Msg, Req, St)
+	end, Msg, Req0, State)of
+		{ ok, NewReq, NewState } ->
+			{ ok, NewReq, NewState };
+		{ ok, NewReq, NewState, hibernate } ->
+			{ ok, NewReq, NewState, hibernate };
+		{ {reply, Frames }, NewReq, NewState } ->
+			{ reply, Frames, NewReq, NewState };
+		{ {reply, Frames }, NewReq, NewState, hibernate } ->
+			{ reply, Frames, NewReq, NewState, hibernate };
+		{ shutdown, NewReq, NewState } ->
+			{ shutdown, NewReq, NewState }
 	end.
 
-terminate(Reason, Req, State) ->
-	Module = module(State),
-  Module:terminate(Reason, Req, st(State)).
 
+terminate(Reason, Req, State) ->
+	lists:foreach(fun(Module) ->
+  	Module:terminate(Reason, Req, st(State))
+  end, State#state.modules).
 
 tick(State) ->
-	erlang:send_after(?TICK * 1000, self(), { tick, State#req.ref }),
+	erlang:send_after(?TICK * 1000, self(), { tick, State#state.ref }),
 	State.
 
 module(State) ->
-	State#req.module.
+	State#state.modules.
 
 st(State) ->
-	State#req.state.
+	State#state.states.
 
 set_st(St, State) ->
-  State#req{ state = St }.
+  State#state{ states = St }.
+
+apply_cmd(Fun, Cmd, Req, State) ->
+	case apply_cmd(Fun, Cmd, State#state.states, [], Req) of
+		{ Answer, NewReq, ModuleSt } ->
+			{ Answer, NewReq, State#state{
+			  states = ModuleSt
+			}};
+		{ Answer, NewReq, ModuleSt, hibernate } ->
+			{ Answer, NewReq, State#state{
+			  states = ModuleSt
+			}, hibernate}
+	end.
+
+apply_cmd(_Fun, _Cmd, Req, [], Acc) ->
+	{ ok, Req, lists:reverse(Acc) };
+
+apply_cmd(Fun, Msg, Req, [{ Module, St} = ModuleItem | ModulesSt ], Acc) ->
+  case Fun(Module, Msg, Req, St) of
+		{ok, NewReq, NewSt } ->
+			apply_cmd(Fun, Msg, Req, ModulesSt, [ ModuleItem | Acc ]);
+		{ok, NewReq, NewSt, hibernate} ->
+			apply_cmd(Fun, Msg, Req, ModulesSt, [ ModuleItem | Acc ]);
+		{reply, Frames, NewReq, NewSt} ->
+			{ { reply, Frames }, NewReq, collect(Module, NewSt, ModulesSt, Acc) };
+		{reply, Frames, NewReq, NewSt, hibernate} ->
+			{ { reply, Frames }, NewReq, collect(Module, NewSt, ModulesSt, Acc), hibernate};
+		{shutdown, NewReq, NewSt} ->
+			{shutdown, NewReq, collect(Module, NewSt, ModulesSt, Acc)}
+	end.
+
+collect(Module, St, ModuleSt, Acc) ->
+  lists:reverse([ { Module, St } | Acc]) ++ ModuleSt.
